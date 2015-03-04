3ce26499914e0ea125d330a182c6a402de7ccb67
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2015-03-04 00:33:36 -0300

    #3 - ajustado url modulo

--- src/main/resources/start-db.sql
+++ src/main/resources/start-db.sql
@@ -1,2 +1,2 @@
 insert into usuario (id, email, senha) values (nextval('usuario_id_seq'), 'teste@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
-insert into aplicacao (id, nome, url, chave) values (nextval('aplicacao_id_seq'), 'Vendas', 'http://localhost:8080/vendas/', 'SACRSC1234');
+insert into aplicacao (id, nome, url, chave) values (nextval('aplicacao_id_seq'), 'Vendas', 'http://localhost:8080/vendas/auth/', 'SACRSC1234');
