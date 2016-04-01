5296f41a9cce9633c551065531b7aafb59aae412
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2016-03-31 21:48:25 -0300

    adicionado configuracao WS faltando

---
+++ relatorios/src/main/java/br/com/emmanuelneri/relatorios/ws/RestApplication.java
@@ -0,0 +1,8 @@
+package br.com.emmanuelneri.relatorios.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("ws")
+public class RestApplication extends Application {
+}
