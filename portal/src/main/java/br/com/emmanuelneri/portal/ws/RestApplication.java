0ba161eedfbc3a7b7379674fc2ba4872ba1b44f4
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2016-04-13 15:33:10 -0300

    removido webservice desnecessario da aplicacao portal

--- portal/src/main/java/br/com/emmanuelneri/portal/ws/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package br.com.emmanuelneri.portal.ws;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("ws")
-public class RestApplication extends Application {
-}
